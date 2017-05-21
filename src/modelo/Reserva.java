72729285f9caca514990dd8c2372a2a1eab76297
Sebastian Dechamps <Sebastian Dechamps@SEBASTIAN-DECHAMPS.home>
2017-05-21 18:24:13 -0300

    resevra

--- src/modelo/Reserva.java
+++ src/modelo/Reserva.java
@@ -1,11 +1,38 @@
 package modelo;
 
-public class Reserva {
+import java.io.Serializable;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(name = "reserva")
+public class Reserva implements Serializable{
+	//SERIALIZABLE.
+	private static final long serialVersionUID = 1L;
+	
+	//ATRIBUTOS.
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", columnDefinition = "SMALLINT")
     private Integer id;
+	@Column(name = "cantidad", columnDefinition = "INTEGER")
     private Integer cantidad;
+	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@Column(name = "slotId", columnDefinition = "SMALLINT")
     private Slot slot;
     
+	
     public Reserva() {
     }
 
--- src/modelo/ReservaOP.java
+++ src/modelo/ReservaOP.java
@@ -1,7 +1,17 @@
 package modelo;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+
 public class ReservaOP extends Reserva {
-    
+    //SERIALIZABLE.
+	private static final long serialVersionUID = 1L;
+	
+	//ATRBIUTOS.
+	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@Column(name = "ordenProduccionId", columnDefinition = "SMALLINT")
 	private OrdenProduccion ordenProduccion;
     
     public ReservaOP() {
