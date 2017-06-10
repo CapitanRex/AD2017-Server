62a2b90f2cdf8a936c3fc1a4e2d60b522fb87daa
CapitanRex <diferrerin@gmail.com>
2017-06-10 19:19:24 -0300

    update 2 10/06/17 almacen

--- src/modelo/ArticuloAlmacenable.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-public interface ArticuloAlmacenable {
-
-    public Boolean sos(Prenda prenda, Talle talle, Color color);
-    public Boolean sos(Insumo insumo);
-    public Integer getLoteId();
-
-}
---
+++ src/modelo/Estado.java
@@ -0,0 +1,51 @@
+package modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="estado")
+public class Estado implements Serializable{
+	//SERIALIZABLE.
+	private static final long serialVersionUID = 1L;
+	//ATRIBUTOS.
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id", columnDefinition="SMALLINT")
+    private Integer id;
+	@Column(name="descripcion", columnDefinition="VARCHAR")
+	private String descripcion;
+	
+	//CONSTRUCTOR VACIO.
+    public Estado() {
+		super();
+	}
+    //CONSTRUCTOR
+	public Estado(Integer id, String descripcion) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}	
+}
